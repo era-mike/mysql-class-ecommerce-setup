a54a55dad0d31f6bcf9e64233c8d45cd336af1bd
Mike <mike@flintavenue.com>
2025-01-17 16:07:28 -0600

    updated sql script, added tasks

---
+++ task1.sql
@@ -0,0 +1,13 @@
+/*
+Task 1: Displaying Product Details
+Scenario: You are building an e-commerce application. One of the features is to display product details to customers.
+
+Task: Write an SQL query to retrieve the following information for all products:
+
+Product name
+Price
+Category
+Availability (In Stock or Out of Stock)
+Include logic to mark products with a stock quantity of 0 as "Out of Stock" and all others as "In Stock."
+
+*/
---
+++ task3.sql
@@ -0,0 +1,10 @@
+/*
+Task 3: Featured Product View
+Scenario: For the homepage, you want to display a featured product.
+
+Task: Write an SQL query to fetch the most expensive product in each category, showing:
+
+Product name
+Price
+Category
+*/
